ab6da2a99e7f975603181a384de8dbcbd98079c7
Glynn Bird <glynn.bird@gmail.com>
2016-12-06 08:37:27 +0000

    1.15.0 - debugging

---
+++ test/debug.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var SERVER = 'https://myaccount.cloudant.com';
+var mydb = 'MYDB';
+var nock = require('nock');
+
+describe('debug', function() {
+
+  it('should work with debug on', function(done) {
+    var reply = { };
+    process.env.DEBUG = 'simplenosql'
+    var mocks = nock(SERVER)
+      .get('/' + mydb).reply(200, reply);
+    var db = require('../index.js')(SERVER, mydb);
+    db.info().then(function() {
+      done();
+    });
+  });
+
+  it('should work with debug off', function(done) {
+    var reply = { };
+    process.env.DEBUG = ''
+    var mocks = nock(SERVER)
+      .get('/' + mydb).reply(200, reply);
+    var db = require('../index.js')(SERVER, mydb);
+    db.info().then(function() {
+      done();
+    });
+  });
+});
