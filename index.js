f3c515f161c4d652895f5a2b0989320fc1bf9f9c
Glynn Bird <glynn.bird@gmail.com>
2018-02-09 11:57:38 +0000

    add standard js linting (#23)
    * add standard js linting
    
    * added standard dependency

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var url = require('url');
+var url = require('url')
 
-module.exports = function(u, dbname) {
-  var parsed = url.parse(u);
+module.exports = function (u, dbname) {
+  var parsed = url.parse(u)
   if (!parsed.hostname || !parsed.protocol) {
-    throw new Error('invalid url');
+    throw new Error('invalid url')
   }
   if (parsed.pathname === '/' && !dbname) {
-    throw new Error('no database name provided');
+    throw new Error('no database name provided')
   }
   if (!dbname) {
-    dbname = parsed.pathname.replace(/^\//,'');
-  } 
-  delete parsed.pathname;
-  delete parsed.path;
-  u = url.format(parsed).replace(/\/$/,'');
-  return require('./lib/db.js')(u, dbname);  
-};
+    dbname = parsed.pathname.replace(/^\//, '')
+  }
+  delete parsed.pathname
+  delete parsed.path
+  u = url.format(parsed).replace(/\/$/, '')
+  return require('./lib/db.js')(u, dbname)
+}
